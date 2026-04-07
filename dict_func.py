76edcb9948e80eadab8441f9502e6ab2254ba69b
Mariana F <marianafama@unisabana.edu.co>
2026-04-07 18:44:19 -0500

    5

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,8 @@
-# Write your code here!
+def employee_print(employee_info):
+    info = employee_info.copy()
+
+    name   = info.pop("Name",   "N/A")
+    salary = info.pop("Salary", "N/A")
+    role   = info.pop("Role",   "N/A")
+
+   
