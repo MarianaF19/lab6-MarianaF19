9bf606e6ae1aabdaf61a8f681fdb4f84829fa015
Mariana F <marianafama@unisabana.edu.co>
2026-04-07 18:47:05 -0500

    5

--- dict_func.py
+++ dict_func.py
@@ -5,4 +5,12 @@ def employee_print(employee_info):
     salary = info.pop("Salary", "N/A")
     role   = info.pop("Role",   "N/A")
 
-   
+    print(f"Name: {name}")
+    print(f"Salary: {salary}")
+    print(f"Role: {role}")
+
+    if info:
+        for key, value in info.items():
+            print(f"{key}: {value}")
+    else:
+        print("No other info!")
